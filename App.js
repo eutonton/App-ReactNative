9945bf5bdb5678b64528cccd159ba90dd7e5fac8
Wellington Araújo <jwellington427@gmail.com>
2024-10-27 23:59:48 -0300

    Mensagem do commit

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Image, StyleSheet, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  const fetchPokemons = async () => {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10');
+      const pokemonData = await Promise.all(
+        response.data.results.map(async (pokemon) => {
+          const pokemonDetails = await axios.get(pokemon.url);
+          return pokemonDetails.data;
+        })
+      );
+      setPokemons(pokemonData);
+    } catch (error) {
+      console.error("Erro ao buscar dados: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.pokemonContainer}>
+      <Image source={{ uri: item.sprites.front_default }} style={styles.avatar} />
+      <View>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.info}>Height: {item.height}</Text>
+        <Text style={styles.info}>Weight: {item.weight}</Text>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={pokemons}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+        />
+      )}
     </View>
   );
 }
@@ -14,7 +57,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  pokemonContainer: {
+    flexDirection: 'row',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  avatar: {
+    width: 64,
+    height: 64,
+    marginRight: 16,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 14,
+    color: '#666',
   },
 });
